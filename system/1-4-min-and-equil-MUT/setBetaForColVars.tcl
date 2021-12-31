9a69259bc30b973e30b8d99b3f84a962a9d393dc
GregorySchwing <gschwing@uno.edu>
2021-12-31 13:50:19 -0600

    make dirs for min mut and wt

---
+++ system/1-4-min-and-equil-MUT/setBetaForColVars.tcl
@@ -0,0 +1,24 @@
+set input_pdb_psf_file_name GCMC_water_O2_myoglobin_MUT_ions 
+set output_file_name MYO 
+
+# load 
+set system [mol new $input_pdb_psf_file_name.psf waitfor all]
+mol addfile $input_pdb_psf_file_name.pdb mol $system waitfor all
+
+set all [atomselect top all]
+$all set beta 0.0
+
+set colVarRef [atomselect top "protein and alpha"]
+$colVarRef set beta 2.0
+
+set fixInGOMC [atomselect top "protein and not alpha and noh"]
+$fixInGOMC set beta 1.0
+
+set wat [atomselect top "water or resname DIOX"]
+$wat set beta 0.0
+
+set ions [atomselect top "ions"]
+$ions set beta 2.0
+
+$all writepdb $output_file_name.pdb
+$all writepsf $output_file_name.psf
---
+++ system/1-4-min-and-equil-WT/createRestraintsFile.tcl
@@ -0,0 +1,19 @@
+set input_pdb_psf_file_name GCMC_water_O2_myoglobin_WT_ions 
+set output_restraints_file_name prot_posres.ref 
+
+# load 
+set system [mol new $input_pdb_psf_file_name.psf waitfor all]
+mol addfile $input_pdb_psf_file_name.pdb mol $system waitfor all
+
+set all [atomselect top all]
+$all set beta 0.0
+
+set fullRest [atomselect top "protein and alpha"]
+$fullRest set beta 1.0
+
+set halfRest [atomselect top "protein and not alpha and noh"]
+$halfRest set beta 0.5
+
+$all writepdb restraints/$output_restraints_file_name
+
+
