7dcd59a26f639add45c3346d90898249293ce266
GregorySchwing <gschwing@uno.edu>
2021-12-31 12:45:53 -0600

    fix cryst mut and added build mutatant psf

---
+++ system/1-1-build/BuildMutant.tcl
@@ -0,0 +1,33 @@
+package require psfgen
+topology top_all36_prot.rtf
+topology toppar_all36_prot_heme.str
+# Chen et al,
+pdbalias residue HIS HSE
+pdbalias residue HEM HEME
+pdbalias atom ILE CD1 CD
+
+segment MYO {
+	first ACE
+	last CTER
+	pdb ./myoglobin.pdb
+	# Neccessary for patch
+	mutate 93 HSD
+	# Mytate back to native human from mutant crystal form
+	mutate 45 LYS
+	# His98Tyr Mutation
+	mutate 97 TYR
+}
+
+segment HEME {
+	pdb ./heme.pdb
+}
+
+patch PHEM MYO:93 HEME:154
+
+coordpdb ./myoglobin.pdb MYO
+coordpdb ./heme.pdb HEME
+
+guesscoord
+
+writepdb ./MYO_HEME.pdb
+writepsf ./MYO_HEME.psf
--- system/1-1-build/BuildPSF.tcl
+++ system/1-1-build/BuildPSF.tcl
@@ -12,28 +12,20 @@ segment MYO {
 	pdb ./myoglobin.pdb
 	# Neccessary for patch
 	mutate 93 HSD
+	# Mytate back to native human from mutant crystal form
+	mutate 45 LYS
 }
 
 segment HEME {
 	pdb ./heme.pdb
 }
-#Fixes heme at distance for O2 to coordinate with HIS 63
-#patch FHEM HEME:154
-
-#patch PHEM MYO:64 HEME:154 
-#segment SO4 {
-#	pdb ./sulfate.pdb
-#}
 
 patch PHEM MYO:93 HEME:154
 
 coordpdb ./myoglobin.pdb MYO
 coordpdb ./heme.pdb HEME
 
-
-#coordpdb ./sulfate.pdb SO4
 guesscoord
-#writepdb ./MYO_HEME_SO4.pdb
-#writepsf ./MYO_HEME_SO4.psf
+
 writepdb ./MYO_HEME.pdb
 writepsf ./MYO_HEME.psf
