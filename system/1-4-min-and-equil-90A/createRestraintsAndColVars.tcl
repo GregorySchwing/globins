967e42841949ea3ad31656242e775ef37633de93
GregorySchwing <gschwing@uno.edu>
2021-12-30 18:19:48 -0600

    Use occupancy for col vars so GOMC can use beta

--- system/1-4-min-and-equil-90A/createRestraintsAndColVars.tcl
+++ system/1-4-min-and-equil-90A/createRestraintsAndColVars.tcl
@@ -9,13 +9,19 @@ mol addfile $input_pdb_psf_file_name.pdb mol $system waitfor all
 set all [atomselect top all]
 $all set beta 0.0
 
+set GOMCFIXED [atomselect top "protein or heme"]
+$GOMCFIXED set beta 1.0
+
+set GOMCFIXEDINBOX [atomselect top "ions"]
+$GOMCFIXEDINBOX set beta 2.0
+
 set fullRest [atomselect top "protein and alpha"]
-$fullRest set beta 1.0
+$fullRest set occupancy 1.0
 
 $all writepdb restraints/$output_colvars_file_name
 
 set halfRest [atomselect top "protein and not alpha and noh"]
-$halfRest set beta 0.5
+$halfRest set occupancy 0.5
 
 $all writepdb restraints/$output_restraints_file_name
 
