f799a78ae075d932524f0174644150e95790cab7
GregorySchwing <gschwing@uno.edu>
2021-12-31 00:49:44 -0600

    per https://www.charmm.org/ubbthreads/ubbthreads.php?ubb=showthreaded&Number=7015 PHEM requires HSD not HSE

--- system/1-1-build/BuildPSF.tcl
+++ system/1-1-build/BuildPSF.tcl
@@ -1,25 +1,33 @@
 package require psfgen
 topology top_all36_prot.rtf
 topology toppar_all36_prot_heme.str
-pdbalias residue HIS HSE
+pdbalias residue HIS HSD
 pdbalias residue HEM HEME
 pdbalias atom ILE CD1 CD
+
 segment MYO {
 	first ACE
 	last CTER
 	pdb ./myoglobin.pdb
 }
+
 segment HEME {
 	pdb ./heme.pdb
 }
 #Fixes heme at distance for O2 to coordinate with HIS 63
-patch PHEM MYO:93 HEME:154 
+#patch FHEM HEME:154
+
 #patch PHEM MYO:64 HEME:154 
 #segment SO4 {
 #	pdb ./sulfate.pdb
 #}
+
+patch PHEM MYO:93 HEME:154
+
 coordpdb ./myoglobin.pdb MYO
 coordpdb ./heme.pdb HEME
+
+
 #coordpdb ./sulfate.pdb SO4
 guesscoord
 #writepdb ./MYO_HEME_SO4.pdb
