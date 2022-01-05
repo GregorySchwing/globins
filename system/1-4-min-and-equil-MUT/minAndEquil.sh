588105b34843599fccaa7e148630fda12c3aab33
GregorySchwing <gschwing@uno.edu>
2022-01-05 00:17:59 -0600

    Revert "fix threadS"
    This reverts commit 4c9e9dc8933eba4709d2e8bb6097851546105140.

--- system/1-4-min-and-equil-MUT/minAndEquil.sh
+++ system/1-4-min-and-equil-MUT/minAndEquil.sh
@@ -5,7 +5,7 @@
 #SBATCH -A greg
 #
 # Number of cores
-#SBATCH -c 4 -w, --nodelist=potoff35
+#SBATCH -c 6 -w, --nodelist=potoff33
 #
 # Runtime of this jobs is less then 12 hours.
 #SBATCH --time=168:00:00
@@ -19,6 +19,6 @@
 
 # Clear the environment from any previously loaded modules
 cd /home6/greg/globins/system/1-4-min-and-equil-MUT
-~/NAMD_2.14_Linux-x86_64-multicore-CUDA/namd2 +p4 step4_equilibration.inp > step4_equilibration.out
+~/NAMD_2.14_Linux-x86_64-multicore-CUDA/namd2 +p6 step4_equilibration.inp > step4_equilibration.out
 
 # End of submit file
