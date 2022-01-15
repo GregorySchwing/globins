f790cf18d609124814a3906dc0ebdc716f125b05
GregorySchwing <gschwing@uno.edu>
2022-01-15 16:00:17 -0600

    A

--- system/gpu.sh
+++ system/gpu.sh
@@ -21,7 +21,6 @@
 #SBATCH -e errors_%j.err
 # Set maximum time limit
 #SBATCH -t 14-0:0:0
-cd XXX
 python3 run_NAMD_GOMC.py -f user_input_NAMD_GOMC.json > log
 python3 combine_data_NAMD_GOMC.py -f user_input_combine_data_NAMD_GOMC.json -w MB 
 
---
+++ system/run5WT.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Job name
+#SBATCH --job-name Simple
+# Submit to the primary QoS
+#SBATCH -q primary
+# Request one node
+#SBATCH -N 1
+# Total number of cores, in this example it will 1 node with 1 core each. 
+#SBATCH -n 1
+# Request memory
+#SBATCH --mem=1G
+# Mail when the job begins, ends, fails, requeues 
+#SBATCH --mail-type=ALL
+# Where to send email alerts
+#SBATCH --mail-user=go2432@wayne.edu
+# Create an output file that will be output_<jobid>.out 
+#SBATCH -o output_%j.out
+# Create an error file that will be error_<jobid>.out
+#SBATCH -e errors_%j.err
+# Set maximum time limit 
+#SBATCH -t 0-0:30:0
+mkdir MT_0
+cp gpu.sh MT_0
+cd MT_0
+sbatch gpu.sh
+
+mkdir MT_1
+cp gpu.sh MT_1
+cd MT_1
+sbatch gpu.sh
+
+mkdir MT_2
+cp gpu.sh MT_2
+cd MT_2
+sbatch gpu.sh
+
+mkdir MT_3
+cp gpu.sh MT_3
+cd MT_3
+sbatch gpu.sh
+
+mkdir MT_4
+cp gpu.sh MT_4
+cd MT_4
+sbatch gpu.sh
