94d4eeae9cbb8648a35391c73d26d24310d0abc9
GregorySchwing <gschwing@uno.edu>
2022-01-17 17:45:56 -0600

    A

--- system/1-6-min-and-equil-WT/setBetaForColVars.tcl
+++ system/1-6-min-and-equil-WT/setBetaForColVars.tcl
@@ -1,9 +1,9 @@
 set input_pdb_psf_file_name_box_0 ../1-5-neutralize/GCMC_water_O2_myoglobin_WT_ions 
 set output_file_name_box_0 LIQ_BOX
 
-# load 
-set system [mol new $input_pdb_psf_file_name.psf waitfor all]
-mol addfile $input_pdb_psf_file_name.pdb mol $system waitfor all
+# load liquid box
+set system [mol new $input_pdb_psf_file_name_box_0.psf waitfor all]
+mol addfile $input_pdb_psf_file_name_box_0.pdb mol $system waitfor all
 
 set all [atomselect top all]
 $all set beta 0.0
