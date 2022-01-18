c45a9cd462315f031a2975d7149c9060906ece9e
GregorySchwing <gschwing@uno.edu>
2022-01-18 11:55:50 -0600

    4 days

--- system/cpu.sh
+++ system/cpu.sh
@@ -18,7 +18,7 @@
 # Create an error file that will be error_<jobid>.out
 #SBATCH -e errors_%j.err
 # Set maximum time limit
-#SBATCH -t 3-0:0:0
+#SBATCH -t 4-0:0:0
 python3 run_NAMD_GOMC.py -f user_input_NAMD_GOMC_CPU.json > log
 python3 combine_data_NAMD_GOMC.py -f user_input_combine_data_NAMD_GOMC.json -w MB 
 
