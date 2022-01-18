030610cbf3aaa9e6d22042a152091293037c4d02
Gregory J. Schwing <go2432@wayne.edu>
2022-01-17 23:57:00 -0500

    1d sec

--- system/1-6-min-and-equil-WT/minAndEquilGrid.sh
+++ system/1-6-min-and-equil-WT/minAndEquilGrid.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Job name
-#SBATCH --job-name CPU
+#SBATCH --job-name CPU_2
 # Submit to the gpu QoS, the requeue QoS can also be used for gpu's 
-#SBATCH -q primary
+#SBATCH -q secondary
 # Request one node
 #SBATCH -N 1
 # Total number of cores, in this example it will 1 node with 1 core each.
@@ -18,6 +18,6 @@
 # Create an error file that will be error_<jobid>.out
 #SBATCH -e errors_%j.err
 # Set maximum time limit
-#SBATCH --time=7-0:00:00
-/wsu/home/go/go24/go2432/NAMD_2.13_Linux-x86_64-multicore +p8 step4_equilibration.inp > step4_equilibration.out
+#SBATCH --time=1-0:00:00
+/wsu/home/go/go24/go2432/NAMD_2.13_Linux-x86_64-multicore/namd2 +p8 step4_equilibration.inp > step4_equilibration.out
 # End of submit file
