9a15b2739fd393fe0edc82ad03541b58acdd7e51
GregorySchwing <gschwing@uno.edu>
2022-01-21 00:18:01 -0600

    neutral

--- system/1-5-neutralize/autoionize_OPEN_MUT .tcl
+++
@@ -1,8 +0,0 @@
-set input_pdb_psf_file_name MYO_HEME_OPEN_MUT_ALIGNED_SHIFTED_SOLVATED
-set output_pdb_psf_file_name GCMC_water_O2_myoglobin_OPEN_MUT_ions
-
-autoionize -psf ../1-4-solvate-protein/${input_pdb_psf_file_name}.psf -pdb ../1-4-solvate-protein/${input_pdb_psf_file_name}.pdb -neutralize
-
-set sel [atomselect top all]
-$sel writepdb ${output_pdb_psf_file_name}.pdb
-$sel writepsf ${output_pdb_psf_file_name}.psf
