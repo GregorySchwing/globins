ccd087cb510ce343621de7ee32621e728ef123a4
GregorySchwing <gschwing@uno.edu>
2022-01-21 23:21:29 -0600

    ABF

---
+++ system/2-5-production-SMD-OPEN_WT/setBetaForColVars.tcl
@@ -0,0 +1,33 @@
+set input_pdb_psf_file_name_box_0 ../2-3-min-and-equil-implicit-MUT/LIQ_BOX 
+set input_bin_file_name_box_0 ../2-3-min-and-equil-implicit-MUT/LIQ_BOX_equilibrated.restart
+set output_file_name_box_0 LIQ_BOX_EQ
+
+# load liquid box
+set system [mol new $input_pdb_psf_file_name_box_0.psf waitfor all]
+mol addfile $input_pdb_psf_file_name_box_0.pdb mol $system waitfor all
+set pdbCRYST [molinfo $system get {a b c alpha beta gamma}]
+mol addfile $input_bin_file_name_box_0.coor mol $system waitfor all
+molinfo $system set {a b c alpha beta gamma} $pdbCRYST
+
+set all [atomselect top all frame 1] 
+
+$all set beta 0.0
+$all set occupancy 0.0
+
+set wat [atomselect top "water or resname DIOX"]
+$wat set beta 0.0
+
+set fullRest [atomselect top "protein and alpha"]
+$fullRest set beta 1.0
+
+set ions [atomselect top "ions"]
+$ions set beta 2.0
+
+set diox [atomselect top "resname DIOX"]
+$diox set occupancy 1.0
+
+set FE [atomselect top "name FE"]
+$FE set occupancy 2.0
+
+$all writepdb $output_file_name_box_0.pdb
+$all writepsf $output_file_name_box_0.psf
